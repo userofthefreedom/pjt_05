835b27bc200f30c2ff1a003ac0e772214aed11cc
sejinoh <sptrain6@naver.com>
2026-04-24 16:11:26 +0900

    feat: add user profile page

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path
 from . import views
+from . import profile_views
 
 app_name = "accounts"
 
 urlpatterns = [
     path("signup/", views.signup, name="signup"),
     path("login/", views.login_view, name="login"),
+
+
+
+    path("profile/", profile_views.profile, name="profile"),
 ]
